dfd7aa0b9463de7e1526e743dab02f2958bde5f4
赵世家 <simon.zhao@milesight.com>
2024-10-23 09:14:59 +0800

    chore(device): update parameters name

--- iab-services/device/device-api/src/main/java/com/milesight/iab/device/facade/IDeviceFacade.java
+++ iab-services/device/device-api/src/main/java/com/milesight/iab/device/facade/IDeviceFacade.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IDeviceFacade {
     List<DeviceNameDTO> fuzzySearchDeviceByName(String name);
 
-    List<DeviceNameDTO> getDeviceNameByIntegrations(List<String> integrations);
+    List<DeviceNameDTO> getDeviceNameByIntegrations(List<String> integrationIds);
 
-    List<DeviceNameDTO> getDeviceNameByIds(List<Long> ids);
+    List<DeviceNameDTO> getDeviceNameByIds(List<Long> deviceIds);
 }
