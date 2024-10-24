3191fea8effc7a16f808c56c8483f541cf53f34b
余绍龙 <yusl@milesight.com>
2024-10-24 16:43:07 +0800

    feat: 修改postgres表字段类型

--- iab-services/authentication/authentication-service/src/main/java/com/milesight/iab/authentication/config/OAuth2Properties.java
+++ iab-services/authentication/authentication-service/src/main/java/com/milesight/iab/authentication/config/OAuth2Properties.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 /**
  * @author loong
  * @date 2024/10/24 14:39
@@ -15,6 +13,6 @@ import java.util.List;
 @ConfigurationProperties(prefix = "oauth2")
 public class OAuth2Properties {
 
-    private List<String> permitUrls;
+    private String[] permitUrls;
 
 }
