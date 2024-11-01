c8f0e9b1648db9444b3ab30240a6bafea8ed1452
张亮 <zhangliang@yeastar.com>
2024-11-01 18:20:18 +0800

    feat: 修改ExchangePayloadProxy boolean的属性获取方式

--- core/context/src/main/java/com/milesight/beaveriot/context/integration/entity/annotation/AnnotationEntityCache.java
+++ core/context/src/main/java/com/milesight/beaveriot/context/integration/entity/annotation/AnnotationEntityCache.java
@@ -31,10 +31,18 @@ public enum AnnotationEntityCache {
     private Method getGetterMethod(Class<?> clazz, Field field) {
         String fieldName = field.getName();
         String capitalizedFieldName = Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
-        String getterName = (field.getType().equals(boolean.class) || field.getType().equals(Boolean.class))? "is" + capitalizedFieldName : "get" + capitalizedFieldName;
+        String getterName = "get" + capitalizedFieldName;
         try {
             return clazz.getMethod(getterName);
         } catch (NoSuchMethodException e) {
+            if (field.getType().equals(boolean.class) || field.getType().equals(Boolean.class)){
+                try {
+                    String booleanGetterName = "is" + capitalizedFieldName;
+                    return clazz.getMethod(booleanGetterName);
+                } catch (NoSuchMethodException ex) {
+                    log.info("No getter method found for field :" + fieldName + ",This cannot be used to receive ExchangePayload data");
+                }
+            }
             log.info("No getter method found for field :" + fieldName + ",This cannot be used to receive ExchangePayload data");
         }
         return null;
