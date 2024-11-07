53e55c720445dfeaa1173a8a533172f09a5e8274
张亮 <zhangliang@yeastar.com>
2024-11-07 19:35:51 +0800

    feat: 启用异步注解

--- application/application-standard/src/main/java/com/milesight/beaveriot/StandardApplication.java
+++ application/application-standard/src/main/java/com/milesight/beaveriot/StandardApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author leon
  */
+@EnableAsync
 @EnableJpaAuditing
 @EnableJpaRepositories(repositoryBaseClass = BaseJpaRepositoryImpl.class )
 @SpringBootApplication
